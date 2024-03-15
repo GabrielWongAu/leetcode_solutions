71eb7911dff9dfc4fda568120fafdb442d102172
Gabriel Wong <gabriel.wong.au@gmail.com>
2024-03-15 21:47:49 +1100

    add two sum solution

---
+++ python/hashmap_problems/0001-two-sum.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}  # val -> index 
+        for index, num in enumerate(nums):
+            diff = target - num
+            if diff in num_dict:
+                return [num_dict[diff],index]
+            num_dict[num] = index
+
+# Time complexity: O(n) We traverse the list containing n elements only once.
+# Each lookup only takes O(1) time.
+
+# Space complexity: O(n) The extra space required depends on the number of
+# items stored in the hash table, which stores at most n elements.
