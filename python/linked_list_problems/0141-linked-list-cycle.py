9c58a5d7469021fdeddba6e710be67301e71303c
Gabriel Wong <gabriel.wong.au@gmail.com>
2024-03-16 11:32:42 +1100

    add linked list cycle

---
+++ python/linked_list_problems/0141-linked-list-cycle.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/linked-list-cycle/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+
+# Time complexity: O(n) 
+# Where n is the total number of nodes in the linked list. 
+
+# Space complexity: O(1) 
+# We only use two nodes (slow and fast) so the space complexity is O(1).
